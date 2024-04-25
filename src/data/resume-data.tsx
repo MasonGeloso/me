f493a83bd1637530a95aabc2fd21736899e3c066
Mason Geloso <hone@Masons-Mac-mini.local>
2024-04-24 17:15:27 -0700

    Quick Commit with gg Command

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,259 +20,126 @@ import {
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Mason Geloso",
+  initials: "MG",
+  location: "Newport Beach, CA",
+  locationLink: "https://www.google.com/maps/place/Newport%20Coast",
   about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
+    "Full Stack Software Engineer with a real passion for problem solving and AI systems.",
   summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+    "I am a Full Stack Software Engineer with over ten years of experience specializing in complex, globally distributed systems and AI technologies. My expertise includes crafting sophisticated architectures and leading teams in a collaborative innovation environment. I have a strong technical background in multiple programming languages and frameworks, with a particular focus on large language models and Retriever-Augmented Generation (RAG) architecture. This specialization enhances machine understanding and response generation by integrating retrieval with generative models. I am committed to leveraging these advanced technologies to address real-world challenges and expand the boundaries of what's possible.",
+  avatarUrl: "https://raw.githubusercontent.com/MasonGeloso/MasonGeloso/main/IMG_4027.JPG",
+  personalWebsiteUrl: "https://masongeloso.com",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "mason@honesystems.io",
+    tel: "+19499035535",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/MasonGeloso",
         icon: GitHubIcon,
       },
       {
-        name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
-        icon: LinkedInIcon,
-      },
-      {
         name: "X",
-        url: "https://x.com/BartoszJarocki",
+        url: "https://x.com/HoneAIAgency",
         icon: XIcon,
       },
     ],
   },
-  education: [
-    {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
-    },
-  ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
+      company: "Asahi Intecc USA",
+      link: "https://asahi-inteccusa-medical.com/",
+      badges: ["Medical Devices", "Machine Learning", "Web App Development"],
+      title: "Process Automation Specialist → Full Stack Engineer",
       logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
+      start: "",
+      end: "",
       description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
+        "I developed Robotic Process Automation (RPA) scripts and engineered full stack web applications and analytics platforms to enhance the efficiency of distribution and order processing systems. My work focused on integrating machine learning techniques to optimize workflows and improve decision-making processes.",
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
+      company: "Credit Company",
+      link: "",
+      badges: ["Financial Industry", "Vue3", "Typescript", "Global Deployment"],
+      title: "Full Stack Developer",
       logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
+      start: "",
+      end: "",
       description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
+        " I specialized in full stack development using Vue3 and TypeScript, crafting robust backend solutions and integrating AI features to enhance global financial services. My role involved developing and deploying scalable applications tailored to meet the dynamic needs of the global market.",
     },
     {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
+      company: "Starlims",
+      link: "https://starlims.com/",
+      badges: ["LLM Development", "RAG Implementation", "AI Consulting"],
+      title: "Software Engineer III",
       logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
+      start: "",
+      end: "",
       description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
+        "Developed innovative AI features that significantly increased revenue by enhancing platform capabilities. Engineered and deployed advanced AI tooling to optimize and streamline internal processes.",
     },
     {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
+      company: "MGi Strategies, LLP",
+      link: "https://www.mgi-strategies.com",
+      badges: ["Engineering Leadership", "Product Development"],
+      title: "CTO & CoFounder",
       logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
+      start: "",
+      end: "",
+      description: "Led the development and strategic direction of engineering initiatives, overseeing the creation of cutting-edge products. Lead the creation of AI powered portfolio management platform.",
     },
   ],
   skills: [
-    "JavaScript",
     "TypeScript",
-    "React/Next.js/Remix",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "React/Next.js/Vue",
+    "Python",
+    "LLM Development",
+    "Engineering Leadership",
+    "Product Management & Development",
+    "Building Scalable Systems",
+    "AWS/GPC/DO",
   ],
   projects: [
     {
-      title: "Consultly",
+      title: "Artify Self",
       techStack: [
         "Side Project",
         "TypeScript",
-        "Next.js",
         "Vite",
-        "GraphQL",
-        "WebRTC",
+        "Directus",
+        "Stable Diffusion",
+        "Dreambooth",
       ],
-      description: "A platform to build and grow your online business",
+      description: "A web app to generate custom images using base images to train a Dreambooth model",
       logo: ConsultlyLogo,
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
+        label: "artify-self.com",
+        href: "https://artify-self.com",
       },
     },
     {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
+      title: "AI Powered Ukrainian Update Videos",
+      techStack: ["Side Project", "Python", "AI", "LLM Development"],
       description:
-        "Browser extension that records everything happening in a web application",
+        "Created a web app to automatically generate AI UKR war update videos based on news feeds",
       logo: MonitoLogo,
       link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
+        label: "@ukraine-daily-updates",
+        href: "https://www.youtube.com/@ukraine-daily-updates",
       },
     },
     {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
+      title: "Aki",
+      techStack: ["Side Project", "AI", "Web Scraping", "Playwright", "Typescript"],
       description:
-        "My personal website and blog. Built with Next.js and Notion API",
+        "Created a web app to automatically find if an Amazon product is being drop-shipped from AliExpress, Alibaba, etc.",
       logo: JarockiMeLogo,
       link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
+        label: "Aki",
+        href: "https://aki.honesystems.io",
       },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
-      link: {
-        label: "github.com",
-        href: "https://parabol.co/",
-      },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
     },
   ],
 } as const;
